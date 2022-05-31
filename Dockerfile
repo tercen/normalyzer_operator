052efb5362b3fea0689d7f4bd2e4ea2a11f84e38
agouy <contact@agouy.com>
2022-05-31 06:49:01 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM tercen/runtime-flowsuite:3.15-1
+
+COPY . /operator
+WORKDIR /operator
+
+RUN R -e "BiocManager::install('NormalyzerDE')"
+
+ENV TERCEN_SERVICE_URI https://tercen.com
+
+ENTRYPOINT ["R", "--no-save", "--no-restore", "--no-environ", "--slave", "-f", "main.R", "--args"]
+CMD ["--taskId", "someid", "--serviceUri", "https://tercen.com", "--token", "sometoken"]
--- main.R
+++ main.R
@@ -1,46 +1,37 @@
 library(tercen)
 library(dplyr)
+library(NormalyzerDE)
 
-# creating an operator library 
-# setRepositories() (use 1 9 10 11 12 13)
-# packrat::init(options = list(use.cache = TRUE))
-# options(repos = c(getOption("repos"), BiocInstaller::biocinstallRepos()))
-ctx = tercenCtx()
+ctx <- tercenCtx()
 
 # select_type <- "none"
-select_type <- ctx$op.value('method')
+select_type <- ctx$op.value('method', as.character, "mean")
 
-noNorm <- function(rawData){rawData}
+noNorm <- function(rawData) {rawData}
 
-norm_func <- switch(select_type,
-                    "global intensity" = NormalyzerDE::globalIntensityNormalization,
-                    "median" = NormalyzerDE::medianNormalization,
-                    "mean" = NormalyzerDE::meanNormalization,
-                    "vsn" = NormalyzerDE::performVSNNormalization,
-                    "quantile" =  NormalyzerDE::performQuantileNormalization,
-                    "smad" = NormalyzerDE::performSMADNormalization,
-                    "cyclicLoess" = NormalyzerDE::performCyclicLoessNormalization,
-                    "global RLR" = NormalyzerDE::performGlobalRLRNormalization,
-                    "none" = noNorm
+norm_func <- switch(
+  select_type,
+  "global intensity" = NormalyzerDE::globalIntensityNormalization,
+  "median" = NormalyzerDE::medianNormalization,
+  "mean" = NormalyzerDE::meanNormalization,
+  "vsn" = NormalyzerDE::performVSNNormalization,
+  "quantile" =  NormalyzerDE::performQuantileNormalization,
+  "smad" = NormalyzerDE::performSMADNormalization,
+  "cyclicLoess" = NormalyzerDE::performCyclicLoessNormalization,
+  "global RLR" = NormalyzerDE::performGlobalRLRNormalization,
+  "none" = noNorm
 )
 
 # options( "tercen.workflowId" = "09f761309ae90820aac2581211009bff")
 # options( "tercen.stepId" = "6-4" )
 
+norm_data <- ctx %>% as.matrix(fill = NA) %>%
+  norm_func()
 
-data = select(ctx, .y, .ci, .ri )
-data = reshape2::acast(data, .ri ~ .ci, value.var='.y', fun.aggregate=mean)
-data[is.nan( data )] <- NA
-norm_data <- norm_func(data)
-
-
-output_df = data.frame(
+df_out <- data.frame(
   norm = as.vector(norm_data),
-  .ci  = rep(0:(ncol(norm_data)-1), each=nrow(norm_data)),
-  .ri  = rep(seq.int(from=0,to=nrow(norm_data)-1), ncol(norm_data)))
+  .ci  = rep(0:(ncol(norm_data) - 1), each = nrow(norm_data)),
+  .ri  = rep(seq.int(from = 0, to = nrow(norm_data) - 1), ncol(norm_data))
+) %>% ctx$addNamespace()
 
-# save it to tercen
-
-output_df <- ctx$addNamespace(output_df )
-ctx$save(output_df )
-
+ctx$save(df_out)
